7483270815ecf5f58094d0ad7a98d74a0ae823fd
Robert Cobain <robert.cobain@sportinginsights.com>
2025-06-15 23:12:07 +0100

    Add .gitignore to exclude system/user-specific files

---
+++ PennyPath/PennyPath/PennyPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyPathApp.swift
+//  PennyPath
+//
+//  Created by Robert Cobain on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PennyPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
